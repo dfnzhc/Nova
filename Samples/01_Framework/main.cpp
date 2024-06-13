31bc947f8f3e9250de3aa7eea1f0d32dbeda621e
Su <z69500jqk@qq.com>
2024-06-14 00:11:48 +0800

    添加样例执行项目

---
+++ Samples/01_Framework/main.cpp
@@ -0,0 +1,27 @@
+﻿/**
+ * @File main.cpp
+ * @Author dfnzhc (https://github.com/dfnzhc)
+ * @Date 2024/6/13
+ * @Brief 
+ */
+
+#include <iostream>
+
+#include <Nova/Nova.hpp>
+
+using namespace nova;
+
+int main()
+{
+//    std::unique_ptr<Window> w = std::make_unique<Window>();
+//
+//    if (!w->init(640, 480, "Test Window")) {
+//        LogError("无法创建窗口");
+//        return -1;
+//    }
+//
+//    w->mainLoop();
+//    w->cleanup();
+
+    return 0;
+}
